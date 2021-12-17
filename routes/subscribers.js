468eb28a818b69e419b93c28187e158a77d4b896
TrekHub <teddyomondi001@gmail.com>
2021-12-17 05:52:54 +0300

    adding route endpoints

---
+++ routes/subscribers.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+
+
+//Creating Routes For  Subscribers
+
+
+// 1. Getting All
+
+router.get('/', (req, res) => {
+
+});
+
+// 2. Creating One
+router.post('/:id', (req, res) => {
+
+});
+
+// 3. Updating One
+router.patch('/:id', (req, res) => {
+
+})
+// 4. Deleting One
+router.delete('/:id', (req, res) => {
+
+})
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,7 +1,33 @@
+require('dotenv').config()
+
 const express = require('express')
+const mongoose = require('mongoose')
 
 //create app using express
 const app = express();
+
+
+//connecting to the database
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true})
+
+const db = mongoose.connection
+db.on('error',(error) => console.error(error));
+db.once('open', () => {console.log("Connected to the Database")})
+
+
+//Allow server to accept JSON
+app.use(express.json())
+
+//setting up routes
+const subscribersRoutes = require('./routes/subscribers')
+app.use('/subscribers', subscribersRoutes)
+
+
+
+
+
+
+
 
 
 //Fire up ther server at port 3000
